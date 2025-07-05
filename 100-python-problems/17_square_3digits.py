266bd5ff52f055562f4a80fb50724fb0aa31e68d
ZataraHere <shabazekhan768@gmail.com>
2025-07-05 20:48:58 +0530

    Sum of squares of numbers

---
+++ 100-python-problems/17_square_3digits.py
@@ -0,0 +1,13 @@
+#	Write a program that will take three digits from the user and add the square of each digit.
+
+def sum_of_squares():
+     
+     num1 = float(input("Enter first no.: "))
+     num2 = float(input("Enter second no.: "))
+     num3 = float(input("Enter third no.: "))
+
+     sum = num1**2 + num2**2 + num3**2
+
+     return sum
+
+sum_of_squares()
