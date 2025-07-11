1df0263a18d69d0736eafe699eb24e3199870c13
ZataraHere <shabazekhan768@gmail.com>
2025-07-11 23:19:28 +0530

    sum of n numbers

---
+++ 100-python-problems/24_sum_of_n_no.py
@@ -0,0 +1,11 @@
+# Write a program to find the sum of first n numbers, where n will be provided by the user. 
+# Eg if the user provides n=10 the output should be 55.
+
+def sum_of_n(n):
+    sum = 0
+    for i in range(n+1):
+        sum += i
+
+    return sum
+
+print(" sum of n numbers:", sum_of_n(10))
