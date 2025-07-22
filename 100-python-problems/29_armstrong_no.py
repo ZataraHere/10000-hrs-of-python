a3bc47c4d30b02e27683411f986ae58b540a4c3a
ZataraHere <shabazekhan768@gmail.com>
2025-07-22 16:15:29 +0530

    Armstrong number checker

--- 100-python-problems/29_armstrong_no.py
+++ 100-python-problems/29_armstrong_no.py
@@ -1,23 +1,15 @@
-#	Print all the armstrong numbers in the range of 100 to 1000
-
 def armstrong(num):
-
     dup = num
-    cnt = 0
-    L = []
+    cnt = len(str(num)) 
     sum = 0
-    while num>0:
-        lastdigit = num%10
-        cnt += 1
-        num = num//10
-        L.append(lastdigit)
-    for i in L:
-        sum += i**cnt
 
-    if sum == dup:
-        return True
-    else:
-        return False
-        
-print(armstrong(1634))
+    while num > 0:
+        last_digit = num % 10
+        sum += last_digit ** cnt
+        num = num // 10
 
+    return sum == dup
+
+print(armstrong(153))  # Output: True
+print(armstrong(9474)) # Output: True
+print(armstrong(123))  # Output: False
