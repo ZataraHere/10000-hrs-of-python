567de7a016998a8f339fbd8c7f3332418cb7308e
ZataraHere <shabazekhan768@gmail.com>
2025-07-27 23:50:18 +0530

    Highest common factor of two numbers

--- 100-python-problems/32_HCF.py
+++ 100-python-problems/32_HCF.py
@@ -2,7 +2,22 @@
 
 def hcf():
 
-    num1, num2 = int(input("Enter two no. : "))
-    print(num1,num2)
+    num1= int(input("Enter first no. : "))
+    num2 = int(input("Enter second no. : "))
+    
+    
+    while num1 > 0 and num2 > 0:
+        
+        if num1 > num2:
+            num1 = num1%num2
+
+        else:
+            num2 = num2%num1
+
+    if num1 == 0:
+        print(num2)
+    
+    else:
+        print(num1)
 
 hcf()
